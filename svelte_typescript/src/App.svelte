6c575e3ec2590a9e3b8b6f0b27bacdc2c72ed3d6
MinFTW <kimmins2@outlook.com>
2022-07-31 22:22:46 -0500

    init svelte typescript version

---
+++ svelte_typescript/src/App.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Todos from './lib/Todos.svelte'
+  import Alert from './lib/Alert.svelte'
+  import { todos } from './stores'
+</script>
+
+<Alert />
+<Todos bind:todos={$todos} />
